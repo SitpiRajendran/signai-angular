3c2bc4b0a9dbcd0b8802773ae7a63cdb25b32fa1
Sitpi Rajendran (Mac) <sitpi@icloud.com>
2022-05-02 12:41:02 -0700

    [UPD] - Online Backend

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -5,11 +5,17 @@ import { CookieService } from 'ngx-cookie-service';
 @Injectable({
   providedIn: 'root',
 })
+
+
 export class BackendService {
+
+    public BACKEND_URL = "https://signai.herokuapp.com";
+    //"http://localhost:3001/"
   constructor(private http: HttpClient, private cookieService: CookieService) {}
 
+
   login(mail: string, password: string) {
-    return this.http.post('http://localhost:3001/auth/login', {
+    return this.http.post(this.BACKEND_URL + '/auth/login', {
       email: mail,
       password: password,
     });
@@ -18,21 +24,21 @@ export class BackendService {
   getUserInfo() {
     let token = 'Bearer ' + this.cookieService.get('token');
     console.log(token);
-    return this.http.get('http://localhost:3001/user/info/me', {
+    return this.http.get(this.BACKEND_URL + '/user/info/me', {
       headers: new HttpHeaders({ authorization: token }),
     });
   }
 
   getProjectList() {
     let token = 'Bearer ' + this.cookieService.get('token');
-    return this.http.get('http://localhost:3001/project/list', {
+    return this.http.get(this.BACKEND_URL + '/project/list', {
       headers: new HttpHeaders({ authorization: token }),
     });
   }
 
   getProjectbyId(id: string) {
     let token = 'Bearer ' + this.cookieService.get('token');
-    return this.http.get('http://localhost:3001/project?id=' + id, {
+    return this.http.get(this.BACKEND_URL + '/project?id=' + id, {
       headers: new HttpHeaders({ authorization: token }),
     });
   }
@@ -40,7 +46,7 @@ export class BackendService {
   getProjectListMyCompany(company: string) {
     let token = 'Bearer ' + this.cookieService.get('token');
     return this.http.get(
-      'http://localhost:3001/project/list?company=' + company,
+      this.BACKEND_URL + '/project/list?company=' + company,
       { headers: new HttpHeaders({ authorization: token }) }
     );
   }
@@ -48,7 +54,7 @@ export class BackendService {
 
   createProject(name: string, description: string, departPositionLong: number, departPositionLat: number, departAddress: string, radius: number, contraints: Array<any>, company: string) {
     let token = 'Bearer ' + this.cookieService.get('token');
-    return this.http.post('http://localhost:3001/project', {
+    return this.http.post(this.BACKEND_URL + '/project', {
       name: name,
       description: description,
       departPositionLong: departPositionLong,
