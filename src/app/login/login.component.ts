336d71a13394ace28932396807da6fc6906638cb
Sitpi Rajendran (Mac) <sitpi@icloud.com>
2022-09-11 13:26:32 +0200

    [ADD] - Loader on login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,61 +3,69 @@ import { Router } from '@angular/router';
 import { BackendService } from '../backend.service';
 import { CookieService } from 'ngx-cookie-service';
 import { Title } from '@angular/platform-browser';
- 
+
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass'],
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.sass'],
 })
 export class LoginComponent implements OnInit {
-  public errormsg: string = '';
-  public cookiesaccepted: boolean = false;
-  title = "Connexion - Signai"
+    public errormsg: string = '';
+    public cookiesaccepted: boolean = false;
+    title = "Connexion - Signai"
 
-  constructor(
-    private router: Router,
-    private backend: BackendService,
-    private cookieService: CookieService,
-    private titleService:Title
-  ) {}
+    constructor(
+        private router: Router,
+        private backend: BackendService,
+        private cookieService: CookieService,
+        private titleService: Title
+    ) { }
 
-  ngOnInit(): void {
-    this.titleService.setTitle(this.title);
-    this.cookiesaccepted = this.cookieService.check('cookiesaccepted')
-    if (this.cookieService.check('token'))
-      this.router.navigate(['/dashboard'])
-  }
-
-  acceptcookies():void {
-    this.cookiesaccepted = true;
-    this.cookieService.set('cookiesaccepted', 'true')
-  }
-
-  verifyconnection(mail: string, password: string) {
-    if (!mail || !password) {
-      console.log('vide');
-      this.errormsg = 'Veuillez remplir tous les champs';
-    } else {
-      this.backend.login(mail, password).subscribe({
-        next: (res) => {
-          console.log(res);
-          this.cookieService.set(
-            'token',
-            JSON.parse(JSON.stringify(res)).accessToken
-          );
-          this.cookieService.set(
-            'email',
-            mail
-          );
-          this.router.navigate(['/dashboard']);
-        },
-        error: (error) => {
-          console.error(error.error, error);
-          this.errormsg = error.error;
-        },
-      });
+    ngOnInit(): void {
+        this.titleService.setTitle(this.title);
+        this.cookiesaccepted = this.cookieService.check('cookiesaccepted')
+        if (this.cookieService.check('token'))
+            this.router.navigate(['/dashboard'])
     }
 
-    //   this.router.navigate(['/dashboard'])
-  }
+    acceptcookies(): void {
+        this.cookiesaccepted = true;
+        this.cookieService.set('cookiesaccepted', 'true')
+    }
+
+    verifyconnection(mail: string, password: string) {
+        let button = document.getElementById("seconnecter")
+
+        if (button)
+            button.innerHTML = "<div class='loader'></div>"
+        if (!mail || !password) {
+            console.log('vide');
+            this.errormsg = 'Veuillez remplir tous les champs';
+            if (button)
+                button.innerHTML = "SE CONNECTER"
+        } else {
+            this.backend.login(mail, password).subscribe({
+                next: (res) => {
+                    console.log(res);
+                    this.cookieService.set(
+                        'token',
+                        JSON.parse(JSON.stringify(res)).accessToken
+                    );
+                    this.cookieService.set(
+                        'email',
+                        mail
+                    );
+                    this.router.navigate(['/dashboard']);
+                },
+                error: (error) => {
+                    console.error(error.error, error);
+                    this.errormsg = error.error;
+                    if (button)
+                        button.innerHTML = "SE CONNECTER"
+                },
+            });
+        }
+
+        //   this.router.navigate(['/dashboard'])
+    }
 }
